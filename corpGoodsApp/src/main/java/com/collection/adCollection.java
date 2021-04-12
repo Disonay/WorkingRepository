f44e327b74bf439d24db8c2fe6e28734c807b816
Sergei <kolesniksn123@mail.ru>
2021-04-12 15:56:45 +0300

    add final version of adCollection. adCollection its collection of ads: findById, delById, getter, add methods have been added

--- corpGoodsApp/src/main/java/com/collection/adCollection.java
+++ corpGoodsApp/src/main/java/com/collection/adCollection.java
@@ -1,19 +1,39 @@
-package com.restapi.servlets;
+package com.collection;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class adCollection {
-    private ArrayList<adClass> ads;
-    adClass getById (String id) {
-        return ads.stream().filter(ad -> ad.getId() == id).collect(Collectors.toList()).get(0);
+    private static ArrayList<adClass> ads;
+    public adCollection() {
+        if (ads == null) {
+            ads = new ArrayList<>();
+        }
     }
-    boolean deleteById (String id) {
+    public adClass getById (String id) {
+        System.out.println(ads.size());
+        List<adClass> idAds = ads.stream().filter(ad -> ad.getId().equals(id)).collect(Collectors.toList());
+        System.out.println(idAds);
+        System.out.println(ads.size());
+        return idAds.size() == 1 ? idAds.get(0) : null;
+    }
+    public boolean deleteById (String id) {
         int size = ads.size();
         ads.removeIf(ad -> ad.getId().equals(id));
-        return size == ads.size();
+        return size != ads.size();
     }
-    ArrayList<adClass> getAds() {
+    public ArrayList<adClass> getAds() {
         return ads;
+    }
+
+    public boolean add(adClass ad) {
+        if (ad.validateAd()) {
+            if (getById(ad.getId()) == null) {
+                ads.add(ad);
+                return true;
+            }
+        }
+        return false;
     }
 }
