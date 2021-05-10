76fd0ec119453444eca678c6638701394efe8b4c
Sergei <kolesniksn123@mail.ru>
2021-05-10 12:55:27 +0300

    add static initialization

--- corpGoodsApp/src/main/java/com/collection/AdCollection.java
+++ corpGoodsApp/src/main/java/com/collection/AdCollection.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class AdCollection {
-    private static ArrayList<AdClass> ads;
+    private static ArrayList<AdClass> ads = new ArrayList<>();
     public AdClass getById (String id) {
         System.out.println(ads.size());
         List<AdClass> idAds = ads.stream().filter(ad -> ad.getId().equals(id)).collect(Collectors.toList());
